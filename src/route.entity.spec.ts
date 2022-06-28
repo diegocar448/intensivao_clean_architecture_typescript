224c76a2bc1a41d55511e5760013b3883282c8b0
Diego Cardoso <diegocar448@hotmail.com>
2022-06-28 09:02:05 -0300

    commit

---
+++ src/route.entity.spec.ts
@@ -0,0 +1,31 @@
+import { Route, RouteProps } from "./route.entity";
+
+describe('Route Tests', () => {
+    
+    test('Constructor', () =>  {
+        // const routeProps: RouteProps = {
+        //     title: 'minha rota',
+        //     startPosition: {lat:0, lng:1},
+        //     endPosition: {lat:1, lng:2},
+        // }
+        // const route = new Route(routeProps);
+        // expect(route.props).toStrictEqual({
+        //     ...routeProps, points: []
+        // })
+
+        // routeProps = {
+        //     title: 'minha rota',
+        //     startPosition: {lat:0, lng:1},
+        //     endPosition: {lat:2, lng:3},
+        //     points:[
+        //         {lat:10, lng:11}
+        //     ]
+        // }
+        // route = new Route(routeProps);
+        // expect(route.props).toStrictEqual({
+        //     ...routeProps, points:[
+        //         {lat:10, lng:11}
+        //     ]
+        // })
+    })
+});
