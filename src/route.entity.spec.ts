fcee3d768860e33a33891abdfef3fc648f426e66
Diego Cardoso <diegocar448@hotmail.com>
2022-06-28 11:20:18 -0300

    primeiros teste e funcionalidades

--- src/route.entity.spec.ts
+++ src/route.entity.spec.ts
@@ -1,31 +1,73 @@
-import { Route, RouteProps } from "./route.entity";
+import { LatLng, Route, RouteProps } from "./route.entity";
 
 describe('Route Tests', () => {
     
-    test('Constructor', () =>  {
-        // const routeProps: RouteProps = {
-        //     title: 'minha rota',
-        //     startPosition: {lat:0, lng:1},
-        //     endPosition: {lat:1, lng:2},
-        // }
-        // const route = new Route(routeProps);
-        // expect(route.props).toStrictEqual({
-        //     ...routeProps, points: []
-        // })
+    test('constructor', () =>  {
 
-        // routeProps = {
-        //     title: 'minha rota',
-        //     startPosition: {lat:0, lng:1},
-        //     endPosition: {lat:2, lng:3},
-        //     points:[
-        //         {lat:10, lng:11}
-        //     ]
-        // }
-        // route = new Route(routeProps);
-        // expect(route.props).toStrictEqual({
-        //     ...routeProps, points:[
-        //         {lat:10, lng:11}
-        //     ]
-        // })
+        let routeProps: RouteProps = {
+            title: 'minharota',
+            startPosition: {lat:0, lng: 1},
+            endPosition: {lat:2, lng: 1}            
+        }
+        let route = new Route(routeProps);
+
+        expect(route.props).toStrictEqual({
+            ...routeProps, points: []
+        })
+
+        routeProps = {
+            title: 'minha rota',
+            startPosition: {lat: 0, lng: 1},
+            endPosition: {lat: 2, lng: 3},
+            points:[
+                {lat: 10, lng: 11},
+            ]
+        }
+        route = new Route(routeProps);
+        expect(route.props).toStrictEqual({
+            ...routeProps, points: [
+                {lat: 10, lng: 11},
+            ]
+        })
     })
-});
+
+
+    test('updateTitle method', () => {
+        const routeProps: RouteProps = {
+            title: 'minharota',
+            startPosition: {lat:0, lng: 1},
+            endPosition: {lat:2, lng: 1}            
+        }
+        const route = new Route(routeProps);
+        route.updateTitle('title updated');
+        expect(route.title).toBe('title updated');
+    })
+
+    test('updatePosition method', () => {
+        const routeProps: RouteProps = {
+            title: 'minharota',
+            startPosition: {lat:0, lng: 1},
+            endPosition: {lat:2, lng: 1}            
+        }
+        const route = new Route(routeProps);
+        const startPosition: LatLng = { lat:10, lng:20 };
+        const endPosition: LatLng = { lat:30, lng:40 };
+        route.updatePosition(startPosition, endPosition);
+        expect(route.startPosition).toBe(startPosition);
+        expect(route.endPosition).toBe(endPosition);
+    })
+
+    test('updatePoints method', () => {
+        const routeProps: RouteProps = {
+            title: 'minharota',
+            startPosition: {lat:0, lng: 1},
+            endPosition: {lat:2, lng: 1}            
+        }
+        const route = new Route(routeProps);
+        const points: LatLng[] = [{ lat:10, lng:20 }];
+    
+        route.updatePoints(points);
+        expect(route.points).toHaveLength(1);
+        
+    })
+})
--- src/route.entity.ts
+++ src/route.entity.ts
@@ -10,15 +10,64 @@ export type RouteProps = {
 
 export class Route {
     public props: Required<RouteProps>;
-    constructor( props: RouteProps ){        
+    constructor( props: RouteProps ){             
         this.props = {
             ...props,
-            points: props.points || []
+            points: props.points || [],
         };
     }
 
+    updateTitle(title: string){
+        this.title = title
+        //mudar pra maiusculo
+        //valor alguns caracteres
+        //validacoes
+    }
+
+    updatePosition(startPosition: LatLng, endPosition:LatLng){
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        //mudar pra maiusculo
+        //valor alguns caracteres
+        //validacoes
+    }
+
+    updatePoints(points: LatLng[]){
+        this.points = points;
+        //mudar pra maiusculo
+        //valor alguns caracteres
+        //validacoes
+    }
+
+    
+
+    //get/set
+    get title(){
+        return this.props.title;
+    }
     private set title(value: string){
         this.props.title = value;
+    }
+
+    get startPosition(){
+        return this.props.startPosition;
+    }
+    private set startPosition(value: LatLng){
+        this.props.startPosition = value;
+    }
+
+    get endPosition(){
+        return this.props.endPosition;
+    }
+    private set endPosition(value: LatLng){
+        this.props.endPosition = value;
+    }
+
+    get points(){
+        return this.props.points;
+    }
+    private set points(value: LatLng[]){
+        this.props.points = value;
     }
 }
 
